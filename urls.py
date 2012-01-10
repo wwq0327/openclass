7417caec0726a1cf3478648d7fb8a0493b997ac6
wwq0327 <wwq0327@gmail.com>
2012-01-10 20:57:24 +0800

    admin

--- urls.py
+++ urls.py
@@ -2,20 +2,12 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'', include('demo.urls')),
-    # Examples:
-    # url(r'^$', 'openclass.views.home', name='home'),
-    # url(r'^openclass/', include('openclass.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+		       url(r'^admin/', include(admin.site.urls)),
 )
 
 media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
